36545bcbe45f902b8237b53237eea41ada7f5da3
Lance Carinugan <lance.carinugan@mrjaffesclass.com>
2023-03-01 12:23:00 -0800

    Original

---
+++ TicTacToe-20230301T201820Z-001/TicTacToe/src/main/java/com/mrjaffesclass/tictactoe/TicTacToe.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mrjaffesclass.tictactoe;
+
+/**
+ *
+ * @author student
+ */
+public class TicTacToe {
+    
+    public TicTacToe() {
+        View view = new View();
+        view.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        TicTacToe ttt = new TicTacToe();
+        
+    }
+}
