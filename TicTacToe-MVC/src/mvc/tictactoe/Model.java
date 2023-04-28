2566cb9b105f2cfad9511f2687563467078ea813
Lance Carinugan <lance.carinugan@mrjaffesclass.com>
2023-04-28 12:46:48 -0700

    Still Bugs, Changed Label

--- TicTacToe-MVC/src/mvc/tictactoe/Model.java
+++ TicTacToe-MVC/src/mvc/tictactoe/Model.java
@@ -4,149 +4,150 @@ import com.mrjaffesclass.apcs.messenger.*;
 
 /**
  * The model represents the data that the app uses.
+ *
  * @author Roger Jaffe
  * @version 1.0
  */
 public class Model implements MessageHandler {
 
-  // Messaging system for the MVC
-  private final Messenger mvcMessaging;
+    // Messaging system for the MVC
+    private final Messenger mvcMessaging;
 
-  // Model's data variables
-  private boolean whoseMove;
-  private boolean gameOver;
-  private String[][] board;
+    // Model's data variables
+    private boolean whoseMove;
+    private boolean gameOver;
+    private String[][] board;
 
-  /**
-   * Model constructor: Create the data representation of the program
-   * @param messages Messaging class instantiated by the Controller for 
-   *   local messages between Model, View, and controller
-   */
-  public Model(Messenger messages) {
-    mvcMessaging = messages;
-    this.board = new String[3][3];
-  }
-  
-  /**
-   * Initialize the model here and subscribe to any required messages
-   */
-  public void init() {
-    this.newGame();
-    this.mvcMessaging.subscribe("playerMove", this);
-    this.mvcMessaging.subscribe("newGame", this);
-    this.mvcMessaging.subscribe("isWinner", this);
- 
-  }
-  
-  /**
-   * Reset the state for a new game
-   */
-  private void newGame() {
-    for(int row=0; row<this.board.length; row++) {
-      for (int col=0; col<this.board[0].length; col++) {
-        this.board[row][col] = "";
-      }
-    }
-    this.whoseMove = false;
-    this.gameOver = false;
-    this.mvcMessaging.notify("turnChange", this.whoseMove);
-  }
-  
-  private String isWinner() {
-    int count = 0;
-    // Check the rows
-    for (String[] rows : this.board) {
-        count = 0;
-        if (rows.equals("X")) {
-            count++;
-        }
-        if (rows.equals("O")) {
-            count--;
-        }
-        if (count == 3) {
-            return "X";
-        }
-        else if (count == -3) {
-            return "O";
-        }
+    /**
+     * Model constructor: Create the data representation of the program
+     *
+     * @param messages Messaging class instantiated by the Controller for local
+     * messages between Model, View, and controller
+     */
+    public Model(Messenger messages) {
+        mvcMessaging = messages;
+        this.board = new String[3][3];
     }
 
-    // Check the columns
-    for (int col = 0; col < this.board[0].length; col++) {
-        count = 0;
-        for (int row = 0; row<this.board.length; row++) {
-            if (board[row][col].equals("X")) {
+    /**
+     * Initialize the model here and subscribe to any required messages
+     */
+    public void init() {
+        this.newGame();
+        this.mvcMessaging.subscribe("playerMove", this);
+        this.mvcMessaging.subscribe("newGame", this);
+        this.mvcMessaging.subscribe("newGameClicked", this);
+        this.mvcMessaging.subscribe("isWinner", this);
+
+    }
+
+    /**
+     * Reset the state for a new game
+     */
+    private void newGame() {
+        for (int row = 0; row < this.board.length; row++) {
+            for (int col = 0; col < this.board[0].length; col++) {
+                this.board[row][col] = "";
+            }
+        }
+        this.whoseMove = false;
+        this.gameOver = false;
+        this.mvcMessaging.notify("turnChange", this.whoseMove);
+    }
+
+    private String isWinner() {
+        int count = 0;
+        // Check the rows
+        for (String[] rows : this.board) {
+            count = 0;
+            if (rows.equals("X")) {
                 count++;
             }
-            if (board[row][col].equals("O")) {
+            if (rows.equals("O")) {
                 count--;
             }
             if (count == 3) {
                 return "X";
-            }
-            else if (count == -3) {
+            } else if (count == -3) {
                 return "O";
             }
         }
-    }
-    // Check the diagonals
-    if (!this.board[0][0].equals("")) {
-        if (this.board[0][0].equals(this.board[1][1]) && this.board[1][1].equals(this.board[2][2])) {
-            return this.board[0][0];
-        }
-    }
-    if (!this.board[0][2].equals("")) {
-        if (this.board[0][2].equals(this.board[1][1]) && this.board[1][1].equals(this.board[2][0])) {
-           return this.board[0][2];
-        }
-    }
-    if (count == 9) {
-        return "This game was a tie.";
-    }
-    return "";
-}
 
-  @Override
-  public void messageHandler(String messageName, Object messagePayload) {
-  // Display the message to the console for debugging
-    if (messagePayload != null) {
-      System.out.println("MSG: received by model: "+messageName+" | "+messagePayload.toString());
-    } else {
-      System.out.println("MSG: received by model: "+messageName+" | No data sent");
+        // Check the columns
+        for (int col = 0; col < this.board[0].length; col++) {
+            count = 0;
+            for (int row = 0; row < this.board.length; row++) {
+                if (board[row][col].equals("X")) {
+                    count++;
+                }
+                if (board[row][col].equals("O")) {
+                    count--;
+                }
+                if (count == 3) {
+                    return "X";
+                } else if (count == -3) {
+                    return "O";
+                }
+            }
+        }
+        // Check the diagonals
+        if (!this.board[0][0].equals("")) {
+            if (this.board[0][0].equals(this.board[1][1]) && this.board[1][1].equals(this.board[2][2])) {
+                return this.board[0][0];
+            }
+        }
+        if (!this.board[0][2].equals("")) {
+            if (this.board[0][2].equals(this.board[1][1]) && this.board[1][1].equals(this.board[2][0])) {
+                return this.board[0][2];
+            }
+        }
+        if (count == 9) {
+            return "This game was a tie.";
+        }
+        return "";
     }
-    
-    // playerMove message handler
-    if (messageName.equals("playerMove")) {
-        if (!this.gameOver) {
-      // Get the position string and convert to row and col
-      String position = (String)messagePayload;
-      Integer row = Integer.valueOf(position.substring(0,1));
-      Integer col = Integer.valueOf(position.substring(1,2));
-      // If square is blank...
-      if (this.board[row][col].equals("")) {
-        // ... then set X or O depending on whose move it is
-        if (this.whoseMove) {
-          this.board[row][col] = "X";
+
+    @Override
+    public void messageHandler(String messageName, Object messagePayload) {
+        // Display the message to the console for debugging
+        if (messagePayload != null) {
+            System.out.println("MSG: received by model: " + messageName + " | " + messagePayload.toString());
         } else {
-          this.board[row][col] = "O";
+            System.out.println("MSG: received by model: " + messageName + " | No data sent");
         }
-        this.whoseMove = !this.whoseMove;
-        // Send the boardChange message along with the new board 
-        this.mvcMessaging.notify("boardChange", this.board);
-        this.mvcMessaging.notify("turnChange", this.whoseMove);
-        if (!isWinner().equals("")) {
-            String winner = isWinner();
-            this.mvcMessaging.notify("winner", winner);
-            this.gameOver = true;
-        }
-    }
-      
-    // newGame message handler
-    } else if (messageName.equals("newGame") || messageName.equals("newGameClicked")) {
-      // Reset the app state
-      this.newGame();
-      // Send the boardChange message along with the new board 
-      this.mvcMessaging.notify("boardChange", this.board);
+
+        // playerMove message handler
+        if (messageName.equals("playerMove")) {
+            if (!this.gameOver) {
+                // Get the position string and convert to row and col
+                String position = (String) messagePayload;
+                Integer row = Integer.valueOf(position.substring(0, 1));
+                Integer col = Integer.valueOf(position.substring(1, 2));
+                // If square is blank...
+                if (this.board[row][col].equals("")) {
+                    // ... then set X or O depending on whose move it is
+                    if (this.whoseMove) {
+                        this.board[row][col] = "X";
+                    } else {
+                        this.board[row][col] = "O";
+                    }
+                    this.whoseMove = !this.whoseMove;
+                    // Send the boardChange message along with the new board 
+                    this.mvcMessaging.notify("boardChange", this.board);
+                    this.mvcMessaging.notify("turnChange", this.whoseMove);
+
+                    if (!isWinner().equals("")) {
+                        gameOver = true;
+                        mvcMessaging.notify("gameOver", isWinner());
+                    }
+                }
+
+                // newGame message handler
+        } else if (messageName.equals("newGame") || messageName.equals("newGameClicked")) {
+                // Reset the app state
+                this.newGame();
+                // Send the boardChange message along with the new board 
+                this.mvcMessaging.notify("boardChange", this.board);
             }
         }
     }
--- TicTacToe-MVC/src/mvc/tictactoe/View.java
+++ TicTacToe-MVC/src/mvc/tictactoe/View.java
@@ -1,85 +1,84 @@
 package mvc.tictactoe;
+
 import com.mrjaffesclass.apcs.messenger.*;
 import javax.swing.JButton;
+
 /**
- * MVC Template
- * This is a template of an MVC framework used by APCS for the 
+ * MVC Template This is a template of an MVC framework used by APCS for the
  * LandMine project (and others)
+ *
  * @author Roger Jaffe
  * @version 1.0
- * 
+ *
  */
 public class View extends javax.swing.JFrame implements MessageHandler {
 
-  private final Messenger mvcMessaging;
-  
-  /**
-   * Creates a new view
-   * @param messages mvcMessaging object
-   */
-  public View(Messenger messages) {
-    mvcMessaging = messages;   // Save the calling controller instance
-    initComponents();           // Create and init the GUI components
-  }
-  
-  /**
-   * Initialize the model here and subscribe
-   * to any required messages
-   */
-  public void init() {
-    // Subscribe to messages here
-    this.mvcMessaging.subscribe("boardChange", this);
-    this.mvcMessaging.subscribe("gameOver", this);
-    this.mvcMessaging.subscribe("isWinner", this);
-    this.mvcMessaging.subscribe("resetBoard", this);
-  }
-  
-  @Override
-  public void messageHandler(String messageName, Object messagePayload) {
-    if (messagePayload != null) {
-      System.out.println("MSG: received by view: "+messageName+" | "+messagePayload.toString());
-    } else {
-      System.out.println("MSG: received by view: "+messageName+" | No data sent");
+    private final Messenger mvcMessaging;
+    private boolean gameOver;
+
+    /**
+     * Creates a new view
+     *
+     * @param messages mvcMessaging object
+     */
+    public View(Messenger messages) {
+        mvcMessaging = messages;   // Save the calling controller instance
+        initComponents();           // Create and init the GUI components
     }
-    if (messageName.equals("boardChange")) {
-    // Get the message payload and cast it as a 2D string array since we
-    // know that the model is sending out the board data with the message
-      String[][] board = (String[][])messagePayload;
-    // Now set the button text with the contents of the board
-      jButton1.setText(board[0][0]);
-      jButton2.setText(board[0][1]);
-      jButton3.setText(board[0][2]);
-      jButton4.setText(board[1][0]);
-      jButton5.setText(board[1][1]);
-      jButton6.setText(board[1][2]);
-      jButton7.setText(board[2][0]);
-      jButton8.setText(board[2][1]);
-      jButton9.setText(board[2][2]);
+
+    /**
+     * Initialize the model here and subscribe to any required messages
+     */
+    public void init() {
+        // Subscribe to messages here
+        this.mvcMessaging.subscribe("boardChange", this);
+        this.mvcMessaging.subscribe("gameOver", this);
+        this.mvcMessaging.subscribe("newGame", this);
+        this.mvcMessaging.subscribe("newGameClicked", this);
+        gameOver = false;
     }
-    if (messageName.equals("isWinner")) {
-        if ("tie".equals(messagePayload)) {
-            jLabel2.setText((String)messagePayload);
+
+    @Override
+    public void messageHandler(String messageName, Object messagePayload) {
+        if (messagePayload != null) {
+            System.out.println("MSG: received by view: " + messageName + " | " + messagePayload.toString());
+        } else {
+            System.out.println("MSG: received by view: " + messageName + " | No data sent");
         }
-        else {
-            jLabel2.setText(messagePayload + " wins");
+        if (messageName.equals("boardChange") && !gameOver) {
+            // Get the message payload and cast it as a 2D string array since we
+            // know that the model is sending out the board data with the message
+            String[][] board = (String[][]) messagePayload;
+            // Now set the button text with the contents of the board
+            jButton1.setText(board[0][0]);
+            jButton2.setText(board[0][1]);
+            jButton3.setText(board[0][2]);
+            jButton4.setText(board[1][0]);
+            jButton5.setText(board[1][1]);
+            jButton6.setText(board[1][2]);
+            jButton7.setText(board[2][0]);
+            jButton8.setText(board[2][1]);
+            jButton9.setText(board[2][2]);
+        } else if (messageName.equals("gameOver")) {
+            jLabel2.setText("Game over! " + (messagePayload.toString().equals("draw") ? ("It's a draw!") : (messagePayload.toString() + " Wins!")));
+            gameOver = true;
         }
     }
-}
 
-  /**
-   * Instantiate an object with the field number that was clicked (1 or 2) and
-   * the direction that the number should go (up or down)
-   * @param fieldNumber 1 or 2 for the field being modified
-   * @param direction this.UP (1) or this.DOWN (-1), constants defined above
-   * @return the HashMap payload to be sent with the message
-   */
-
-  /**
-   * This method is called from within the constructor to initialize the form.
-   * WARNING: Do NOT modify this code. The content of this method is always
-   * regenerated by the Form Editor.
-   */
-  @SuppressWarnings("unchecked")
+    /**
+     * Instantiate an object with the field number that was clicked (1 or 2) and
+     * the direction that the number should go (up or down)
+     *
+     * @param fieldNumber 1 or 2 for the field being modified
+     * @param direction this.UP (1) or this.DOWN (-1), constants defined above
+     * @return the HashMap payload to be sent with the message
+     */
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -93,7 +92,7 @@ public class View extends javax.swing.JFrame implements MessageHandler {
         jButton3 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
         jButton9 = new javax.swing.JButton();
-        jButton10 = new javax.swing.JButton();
+        newGameClicked = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -163,7 +162,13 @@ public class View extends javax.swing.JFrame implements MessageHandler {
             }
         });
 
-        jButton10.setText("Reset Game");
+        newGameClicked.setText("newGame");
+        newGameClicked.setName("newGameClicked"); // NOI18N
+        newGameClicked.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                newGameClicked(evt);
+            }
+        });
 
         jLabel2.setText("jLabel2");
 
@@ -194,15 +199,15 @@ public class View extends javax.swing.JFrame implements MessageHandler {
                 .addGap(90, 90, 90)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton10)
-                .addContainerGap())
+                .addComponent(newGameClicked)
+                .addGap(22, 22, 22))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton10)
+                    .addComponent(newGameClicked)
                     .addComponent(jLabel12)
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -234,18 +239,26 @@ public class View extends javax.swing.JFrame implements MessageHandler {
 
     private void onClick(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_onClick
         // TODO add your handling code here:
-        JButton button = (JButton)evt.getSource();
-    this.mvcMessaging.notify("playerMove", button.getName());
+        JButton button = (JButton) evt.getSource();
+        this.mvcMessaging.notify("playerMove", button.getName());
+        if(gameOver){
+            return;
+        }
 
     }//GEN-LAST:event_onClick
 
-  /**
-   * @param args the command line arguments
-   */
+    private void newGameClicked(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_newGameClicked
+        // TODO add your handling code here:
+        this.gameOver = false;
+        this.mvcMessaging.notify("newGameClicked");
+    }//GEN-LAST:event_newGameClicked
+
+    /**
+     * @param args the command line arguments
+     */
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
@@ -256,5 +269,6 @@ public class View extends javax.swing.JFrame implements MessageHandler {
     private javax.swing.JButton jButton9;
     private javax.swing.JLabel jLabel12;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton newGameClicked;
     // End of variables declaration//GEN-END:variables
 }
